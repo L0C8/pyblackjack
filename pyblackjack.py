ee3964b78e6027909d9b87141a842335020c7e57
L0C8 <TimB02@protonmail.com>
2025-02-21 19:33:09 -0500

    added game class

--- pyblackjack.py
+++ pyblackjack.py
@@ -1,6 +1,35 @@
 from colorama import Fore, Back, Style
-print(Fore.RED + 'some red text')
-print(Back.GREEN + 'and with a green background')
-print(Style.DIM + 'and in dim text')
-print(Style.RESET_ALL)
-print('back to normal now')
+import os, random
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+class Deck:
+    def __init__(self):
+        print("Test")
+        
+    def shuffle(self):
+        return 5  
+    
+class Game:
+    score = 0
+    
+    def __init__(self, start):
+        self.score = start
+    
+    def addScore(self, val):
+        self.score += val 
+    
+    def getScore(self):
+        return self.score 
+    
+cards = {"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+suite = {"Hearts", "Clubs", "Spades", "Diamonds"}
+symbol = {"♥", "♣", "♠", "♦"}
+
+#test
+game = Game(25)
+print(game.getScore())
+game.addScore(5)
+print(game.getScore())
+
